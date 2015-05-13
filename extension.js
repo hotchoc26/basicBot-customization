fc7d51813dc8c1114b95a7ebf7b4639ad8e2e165
hotchoc26 <devancoetzee@hotmail.com>
2015-05-13 14:24:16 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -59,7 +59,7 @@
         startupVolume: 0, // 0-100
         startupEmoji: false, // true or false
         cmdDeletion: true,
-        chatLink: "https://raw.githubusercontent.com/hotchoc26/basicBot-customization/master/en.json",
+        chatLink: "https://raw.githubusercontent.com/hotchoc26/basicBot/master/lang/en.json", // "https://raw.githubusercontent.com/hotchoc26/basicBot-customization/master/en.json",
         maximumAfk: 120,
         afkRemoval: true,
         maximumDc: 60,
